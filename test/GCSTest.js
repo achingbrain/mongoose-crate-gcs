f40918f2f8451be8c51769339a5405ceaf16c75b
Markus Voss <markus.voss@ehelse.no>
2017-10-31 20:26:06 +0100

    Added option to set headers in gcs putStream

--- test/GCSTest.js
+++ test/GCSTest.js
@@ -301,4 +301,44 @@ describe('GCS', () => {
     }
     gcs.emit('connected')
   })
+
+  it('should override add options.headers to headers putstream', (done) => {
+    const sourceFile = path.resolve(path.join(__dirname, '.', 'fixtures', 'node_js_logo.png'))
+
+    const client = {
+      putStream: sinon.stub()
+    }
+
+    const nodeGcs = function () {
+      return client
+    }
+    nodeGcs.gapitoken = sinon.stub()
+    nodeGcs.gapitoken.callsArg(1)
+
+    client.putStream.callsArgWith(4, undefined, {request: {}})
+
+    const GCS = proxyquire('../lib/GCS', {
+      'node-gcs': nodeGcs
+    })
+
+    const gcs = new GCS({
+      keyFile: 'foo',
+      iss: 'bar',
+      bucket: 'bucket',
+      headers: {
+        'Cache-Control': 'private'
+      }
+    })
+
+    gcs.save({
+      path: sourceFile,
+      size: 1234,
+      type: 'image/png'
+    }, (error, url) => {
+      should(error).not.ok
+      // headers should contain Cache-Control
+      client.putStream.getCall(0).args[3]['Cache-Control'].should.equal('private')
+      done()
+    })
+  })
 })
